fd565f294be5f5c92b3a24287aca95812cf3a391
Alexander Poole <alex.o.poole@gmail.com>
2022-09-18 11:58:56 +0200

    Redo config from scratch using Lua
    This convers navigation and basic look and feel

---
+++ lua/owodunni/keymaps.lua
@@ -0,0 +1,62 @@
+local opts = { noremap = true, silent = true }
+
+local erm_opts = { silent = true }
+
+-- Shorten function name
+local keymap = vim.api.nvim_set_keymap
+
+--Remap space as leader key
+keymap("", "<Space>", "<Nop>", opts)
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- Modes
+--   normal_mode = "n",
+--   insert_mode = "i",
+--   visual_mode = "v",
+--   visual_block_mode = "x",
+--   term_mode = "t",
+--   command_mode = "c",
+
+-- Normal --
+-- Better window navigation
+keymap("n", "<Down>", "<C-w>j", opts)
+keymap("n", "<Up>", "<C-w>k", opts)
+keymap("n", "<Right>", "<C-w>l", opts)
+keymap("n", "<Left>", "<C-w>h", opts)
+
+keymap("n", "<leader>e", ":Lex 30<cr>", opts)
+
+-- Resize with arrows
+keymap("n", "<C-k>", ":resize +2<CR>", opts)
+keymap("n", "<C-j>", ":resize -2<CR>", opts)
+keymap("n", "<C-h>", ":vertical resize -2<CR>", opts)
+keymap("n", "<C-l>", ":vertical resize +2<CR>", opts)
+
+-- Navigate buffers
+keymap("n", "<A-l>", ":bnext<CR>", opts)
+keymap("n", "<A-h>", ":bprevious<CR>", opts)
+
+-- Visual --
+-- Stay in indent mode
+keymap("v", "<", "<gv", opts)
+keymap("v", ">", ">gv", opts)
+
+-- Move text up and down
+keymap("v", "<Down>", ":m .+1<CR>==", opts)
+keymap("v", "<Up>", ":m .-2<CR>==", opts)
+keymap("x", "<Down>", ":move '>+1<CR>gv-gv", opts)
+keymap("x", "<Up>", ":move '<-2<CR>gv-gv", opts)
+
+-- Keep buffer when replacing text
+keymap("v", "p", '"_dP', opts)
+
+-- Visual Block --
+-- Move text up and down
+
+-- Terminal --
+-- Better terminal navigation
+-- keymap("t", "<C-h>", "<C-\\><C-N><C-w>h", term_opts)
+-- keymap("t", "<C-j>", "<C-\\><C-N><C-w>j", term_opts)
+-- keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
+-- keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
