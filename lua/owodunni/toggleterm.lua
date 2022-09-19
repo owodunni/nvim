77f93e9aa9fdba43b0e52990e0cd5ef391f251f1
Alexander Poole <alex.o.poole@gmail.com>
2022-09-19 19:27:27 +0200

    Exit terminal mode using esc

--- lua/owodunni/toggleterm.lua
+++ lua/owodunni/toggleterm.lua
@@ -22,7 +22,7 @@ toggleterm.setup({
 
 function _G.set_terminal_keymaps()
   local opts = {noremap = true}
-  -- vim.api.nvim_buf_set_keymap(0, 't', '<esc>', [[<C-\><C-n>]], opts)
+  vim.api.nvim_buf_set_keymap(0, 't', '<esc>', [[<C-\><C-n>]], opts)
   vim.api.nvim_buf_set_keymap(0, 't', '<Left>', [[<C-\><C-n><C-W>h]], opts)
   vim.api.nvim_buf_set_keymap(0, 't', '<Down>', [[<C-\><C-n><C-W>j]], opts)
   vim.api.nvim_buf_set_keymap(0, 't', '<Up>', [[<C-\><C-n><C-W>k]], opts)
