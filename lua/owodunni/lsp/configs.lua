f89323f9866b9878835af867680e0418a1edae4f
Alexander Poole <alexanderp@opera.com>
2022-09-20 10:59:16 +0200

    Add custom config for tsserver
    tsserver would try to use local packages as the root packages. This lead
    to it not being able to find references in the entire project. By
    changing to .git as root pattern this is no longer an issue and
    references works as intended.

--- lua/owodunni/lsp/configs.lua
+++ lua/owodunni/lsp/configs.lua
@@ -5,7 +5,7 @@ end
 
 local lspconfig = require("lspconfig")
 
-local servers = { "jsonls", "sumneko_lua", "yamlls", "tsserver", "vuels", "tailwindcss", "html", "bashls" }
+local servers = { "jsonls", "sumneko_lua", "yamlls", "tsserver", "vuels", "bashls" }
 
 lsp_installer.setup({
 	ensure_installed = servers,
