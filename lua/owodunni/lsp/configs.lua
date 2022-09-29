fad56c929b6c34b0ebad34a42ddf8e6a91bdce44
Alexander Poole <alex.o.poole@gmail.com>
2022-09-29 07:52:11 +0200

    Add cssls

--- lua/owodunni/lsp/configs.lua
+++ lua/owodunni/lsp/configs.lua
@@ -5,7 +5,7 @@ end
 
 local lspconfig = require("lspconfig")
 
-local servers = { "jsonls", "sumneko_lua", "yamlls", "tsserver", "vuels", "bashls", "beancount" }
+local servers = { "jsonls", "sumneko_lua", "yamlls", "tsserver", "vuels", "bashls", "beancount", "cssls" }
 
 lsp_installer.setup({
 	ensure_installed = servers,
