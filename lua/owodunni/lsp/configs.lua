1b57f227a7f78249bb9dea85841acccb23b9490a
Alexander Poole <alexanderp@opera.com>
2023-03-06 08:44:01 +0100

    Add eslint and tailwindcss as lsps

--- lua/owodunni/lsp/configs.lua
+++ lua/owodunni/lsp/configs.lua
@@ -5,7 +5,7 @@ end
 
 local lspconfig = require("lspconfig")
 
-local servers = { "jsonls", "lua_ls", "yamlls", "tsserver", "vuels", "svelte", "bashls", "beancount", "cssls" }
+local servers = { "jsonls", "lua_ls", "yamlls", "tsserver", "vuels", "svelte", "eslint", "tailwindcss", "bashls", "beancount", "cssls" }
 
 lsp_installer.setup({
 	ensure_installed = servers,
