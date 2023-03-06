beff9b1850550abf66ed0ee8e6b3c3b181cc9b88
Alexander Poole <alexanderp@opera.com>
2023-03-06 08:32:06 +0100

    Add copilot
    Accept selections by Pressing Alt+l

--- lua/owodunni/plugins.lua
+++ lua/owodunni/plugins.lua
@@ -107,6 +107,9 @@ return packer.startup(function(use)
 	-- Jest
 	use("kubejm/jest.nvim")
 
+	-- Copilot
+	use("zbirenbaum/copilot.lua")
+
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
 	if PACKER_BOOTSTRAP then
