f72336c1f4a032458bd6ed98590aa08ece0bd378
Alexander Poole <alex.o.poole@gmail.com>
2023-04-24 06:55:19 +0200

    Change harpoon menu key

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -9,7 +9,7 @@ end
 harpoon.setup({})
 
 vim.keymap.set("n", "<leader>w", mark.add_file)
-vim.keymap.set("n", "<leader>r", ui.toggle_quick_menu)
+vim.keymap.set("n", "<leader>ww", ui.toggle_quick_menu)
 
 vim.keymap.set("n", "<C-j>", function() ui.nav_next() end)
 vim.keymap.set("n", "<C-k>", function() ui.nav_prev() end)
