bb9edfcda0d52183dfc3e072546ca24283a8551f
Alexander Poole <alex.o.poole@gmail.com>
2023-10-12 07:59:01 +0200

    Add git copy command
    https://stackoverflow.com/a/44036771

--- alias.sh
+++ alias.sh
@@ -149,6 +149,24 @@ function grev () {
     git push --set-upstream critic "$name"
 }
 
+function gcopy () {
+  if [[ $# -ne 2 ]] ; then
+    echo "Usage: gcopy original copy"
+    exit 0
+  fi
+
+  git mv "$1" "$2"
+  git commit -n -m "Split history $1 to $2 - rename file to target-name"
+  REV=`git rev-parse HEAD`
+  git reset --hard HEAD^
+  git mv "$1" temp
+  git commit -n -m "Split history $1 to $2 - rename source-file to temp"
+  git merge $REV
+  git commit -a -n -m "Split history $1 to $2 - resolve conflict and keep both files"
+  git mv temp "$1"
+  git commit -n -m "Split history $1 to $2 - restore name of source-file"
+}
+
 # gradle
 alias gw=gradle
 alias ll='ls -al'
