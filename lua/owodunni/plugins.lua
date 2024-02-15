72063db7952047fb41feb5e626fba9c9aed90961
Alexander Poole <alexanderp@opera.com>
2024-02-15 06:33:09 +0100

    Add mini.animate to get animation when doing half page scroll
    I thought doing half page scrolls was disorienting, googling I found
    that adding animations could be a solution. And indeed it makes the
    experience much more plesent. I read it in this reddit thread:
    
    https://www.reddit.com/r/neovim/comments/1707ppd/anybody_actually_use_ctrld_and_ctrlu_to_scroll/

--- lua/owodunni/plugins.lua
+++ lua/owodunni/plugins.lua
@@ -104,6 +104,9 @@ return packer.startup(function(use)
   use("theprimeagen/harpoon")
   use("nvim-telescope/telescope.nvim")
 
+  -- Animation
+  use({ 'echasnovski/mini.animate', branch = 'stable' })
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
