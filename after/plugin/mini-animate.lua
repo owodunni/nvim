65235baf1dad551f72342c4874eb1c7d591a0cc0
Alexander Poole <alexanderp@opera.com>
2024-02-21 06:39:07 +0100

    Only animate cursor and not scroll

--- after/plugin/mini-animate.lua
+++ after/plugin/mini-animate.lua
@@ -1,6 +1,39 @@
 local status_ok, animate = pcall(require, "mini.animate")
 if not status_ok then
-  return
+	return
 end
 
-animate.setup()
+animate.setup(
+	-- No need to copy this inside `setup()`. Will be used automatically.
+	{
+		-- Cursor path
+		cursor = {
+			-- Whether to enable this animation
+			enable = true,
+		},
+
+		-- Vertical scroll
+		scroll = {
+			-- Whether to enable this animation
+			enable = false,
+		},
+
+		-- Window resize
+		resize = {
+			-- Whether to enable this animation
+			enable = false,
+		},
+
+		-- Window open
+		open = {
+			-- Whether to enable this animation
+			enable = false,
+		},
+
+		-- Window close
+		close = {
+			-- Whether to enable this animation
+			enable = false,
+		},
+	}
+)
